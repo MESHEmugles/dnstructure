69bd1480cc431ee3ce4ef102f0c753eca76f4eb7
phlenfyl <phlenfytest28@gmail.com>
2024-05-25 21:46:45 +0100

    corrected metaandfooter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Desyns Structure LTD",
+  title: "Dezyns Structure LTD",
   description: "We listen and execute",
 };
 
